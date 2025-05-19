bab23a5cbad507cb5d361caed155d25e6b0e36b2
unknown <shilra066@gmail.com>
2025-05-20 05:06:45 +0700

    Update Fitur Soal

---
+++ TempJadwalCode.go
@@ -0,0 +1,22 @@
+func menuJadwal()
+	var inputjadwal int
+
+	fmt.Println(" _________________________________")
+	fmt.Println("|                                 |")
+	fmt.Println("| Aplikasi Asistensi Pembelajaran |")
+	fmt.Println("|_________________________________|")
+	fmt.Println()
+	fmt.Println("===================================")
+	fmt.Println("=   Menu Jadwal Pembelajaran      =")
+	fmt.Println("===================================")
+	fmt.Println("Pilihan Menu")
+	fmt.Println("1. Menambahkan Jadwal")
+	fmt.Println("2. Menghapus Jadwal")
+	fmt.Println("3. Mengubah Jadwal")
+	fmt.Println("4. Menampilkan Jadwal")
+	fmt.Println("5. Mengurutkan Jadwal")
+	fmt.Println("0. Kembali")
+	fmt.Print("Pilih Menu : ")
+
+	fmt.Scan(&inputjadwal)
+}
